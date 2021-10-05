3ca9b59cdd691522da8eb0ceca35ee16aaabab20
jani-siv <info@kengityspaja.fi>
2021-10-05 09:54:43 +0300

    init

---
+++ src/main.cpp
@@ -0,0 +1,31 @@
+//============================================================================
+// Name        : main.cpp
+// Author      : Jani
+// Version     : 0.01
+// Copyright   : Your copyright notice
+// Description : Linux LoRa school project
+//				 p2p connection between 2 raspberry pi 4
+//				 toolchain: arm-none-linux-gnueabihf-
+//				 core: arm cortex a72 cortex a53
+//				 distro: Debian
+//				 LoRa module: Microship rn2483 433Mhz 868Mhz
+//
+//ToDo make library what test and open uart connection between devices
+//ToDo make library what send message
+//ToDo make module commands in own file
+//ToDo make master and slave select in main
+//ToDo create Github repo
+//============================================================================
+
+#include <iostream>
+using namespace std;
+//
+
+
+int
+main()
+{
+	//define uart connection between LoRa module and Raspberry pi
+  cout << "sadf" << endl;
+  return 0;
+}
